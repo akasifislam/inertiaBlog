86915f6cf361e69793db27a2235742c17e3f7524
akasifislam <aseasifislam@gmail.com>
2021-11-17 10:49:14 +0600

    font support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -7,7 +9,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      // fontFamily: {
+      //   custom: ['Inter var', 'sans-serif'],
+      // },
+    },
   },
   variants: {
     extend: {},
